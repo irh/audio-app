0cd2375164f2c83538ab6eb83f2a306132824c46
Ian Hobson <ian.r.hobson@gmail.com>
2025-11-08 23:04:19 +0100

    WIP - Add Android support for the egui app

--- apps/egui/app/src/lib.rs
+++ apps/egui/app/src/lib.rs
@@ -1,2 +1,21 @@
 mod app;
 pub use app::App;
+
+#[cfg(target_os = "android")]
+#[unsafe(no_mangle)]
+fn android_main(app: winit::platform::android::activity::AndroidApp) {
+    use eframe::{NativeOptions, run_native};
+    use log::LevelFilter;
+
+    android_logger::init_once(android_logger::Config::default().with_max_level(LevelFilter::Info));
+
+    run_native(
+        env!("PRODUCT_NAME"),
+        NativeOptions {
+            android_app: Some(app),
+            ..Default::default()
+        },
+        Box::new(|_| Ok(Box::new(App::new()?))),
+    )
+    .unwrap()
+}
